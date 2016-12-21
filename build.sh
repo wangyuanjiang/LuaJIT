0f3320874ee638bd6615873d8a43f07165d265d5
Sen Han <yunthanatos@gmail.com>
2016-12-21 19:43:39 +0800

    test Travis CI helper :)

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo build ":)"
-sudo find / 
+sudo find /  | head
+sudo find / -maxdepth 1 -type d -exec ls -ld "{}" \;
 curl -v http://google.com
 curl -v http://baidu.com
 ping 8.8.8.8
